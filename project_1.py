b3286e7636786cffd7a2c9fadcb1a49708e3b8b0
lavanyabevara05 <lavanyabevara1805@gmail.com>
2026-07-07 15:41:17 +0530

    add project_1

---
+++ project_1.py
@@ -0,0 +1,19 @@
+print("Welcome to AI Chatbot!")
+
+while True:
+    user = input("You: ").lower()
+
+    if user in("hello" , "hi" , "hey"):
+        print("Bot: Hi dear ")
+
+    elif user == "how are you":
+        print("Bot: I'm doing great!")
+
+    elif user == "bye":
+        print("Bot: Goodbye!")
+        break
+
+    else:
+        print("Bot: Sorry, I don't understand.")
+
+
