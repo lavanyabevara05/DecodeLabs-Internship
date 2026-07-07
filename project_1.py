873ceb181d24d228e80118007240c89d43ff8396
lavanyabevara05 <lavanyabevara1805@gmail.com>
2026-07-07 18:31:06 +0530

    add more features

--- project_1.py
+++ project_1.py
@@ -1,20 +1,33 @@
 print("Welcome to AI Chatbot!")
 
+name = "" # if user starts by press enter name is an error so we intialize
 while True:
     user = input("You: ").lower()
 
     if user in("hello" , "hi" , "hey"):
-        print("Bot: Hi dear ")
+        print("AI : Hi dear ! Nice to meet you ")
+        name = input("can u please tell me your sweer name dear: ")
+        print (f"That's Beautiful name {name}")
+
+    elif user == "":
+      print(f"let's talk {name}")
 
     elif user == "how are you":
-        print("Bot: I'm doing great!")
+        print("AI : I'm doing great!")
+
+
+    elif user == "what is python":
+      print("AI : Python is a high-level programming language.")
+
+    elif user =="what is github":
+      print(" AI :  GitHub is an online platform that stores Git repositories")
 
     elif user == "bye":
-        print("Bot: Goodbye!")
+        print(" AI : Thank you! Have a wonderful day.")
         break
+
 
     else:
         print("Bot: Sorry, I don't understand.")
-        
 
 
